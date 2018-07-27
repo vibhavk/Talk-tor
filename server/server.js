28d73b0323ad199863a013814fc2aa791a515de7
vibhavk <vibhavkunj@hotmail.com>
2018-07-28 00:44:05 +0530

    basic setup for websocket.io

--- server/server.js
+++ server/server.js
@@ -1,13 +1,26 @@
 const path = require('path');
-const publicPath = path.join(__dirname,'../public');
+const http = require('http');
 const express = require('express');
+const socketIO = require('socket.io');
+
+const publicPath = path.join(__dirname,'../public');
 
 const port = process.env.PORT || 3000; 
-var app = express();
+var app = express(); 
+// we switched from express server to http server(though the former calls the latter anyway) because we wanted to include socket in the gang!
+var server = http.createServer(app);
+var io = socketIO(server); //we get back a websocket server
 
 app.use(express.static(publicPath));
 
+io.on('connection',(socket)=>{
+    console.log('New user connected! Hi!');
 
-app.listen(port,()=>{
+    socket.on('disconnect',()=>{ //function to run when a socket was disconnected
+        console.log('User disconnected from server');
+    });
+}); //event listener registration, here we are listening for a new connection...socket represents the invidual socket connection
+
+server.listen(port,()=>{
     console.log(`Server is up on port ${port}`);
 });

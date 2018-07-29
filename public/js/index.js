29ad8ee4a61ca8faa08542d1bf29840ad12d87fe
vibhavk <vibhavkunj@hotmail.com>
2018-07-30 01:43:03 +0530

    formatted timestamps using momentjs

--- public/js/index.js
+++ public/js/index.js
@@ -17,15 +17,17 @@ socket.on('disconnect',function(){
 
 socket.on('newMessage',function(message){
     console.log('New message!',message); //custom event-listener!
+    var formattedTime = moment(message.createdAt).format('h:mm A');
     var li = jQuery('<li></li>');
-    li.text(`${message.from} : ${message.text}`);
+    li.text(`${message.from} ${formattedTime}: ${message.text}`);
     jQuery('#messages').append(li)
 });
 
 socket.on('newLocationMessage',function(message){
+    var formattedTime = moment(message.createdAt).format('h:mm A');
     var li = jQuery('<li></li>');
     var a = jQuery('<a target="_blank">My current location<a/>') //_blank opens the link in new tab, so we dont get kicked out of the room
-    li.text(`${message.from}:`);
+    li.text(`${message.from} ${formattedTime}:`);
     a.attr('href',message.url);
     li.append(a);
     jQuery('#messages').append(li)

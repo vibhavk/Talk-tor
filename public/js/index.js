d356d2549d2b4603065d8f933bd51e73c6d7d563
vibhavk <vibhavkunj@hotmail.com>
2018-07-30 15:33:03 +0530

    UI scalability improved using Mustache.js

--- public/js/index.js
+++ public/js/index.js
@@ -18,19 +18,25 @@ socket.on('disconnect',function(){
 socket.on('newMessage',function(message){
     console.log('New message!',message); //custom event-listener!
     var formattedTime = moment(message.createdAt).format('h:mm A');
-    var li = jQuery('<li></li>');
-    li.text(`${message.from} ${formattedTime}: ${message.text}`);
-    jQuery('#messages').append(li)
+    var template = jQuery('#message-template').html();
+    var html = Mustache.render(template,{
+        text:message.text,
+        from:message.from,
+        createdAt:formattedTime
+    });
+    jQuery('#messages').append(html);
 });
 
 socket.on('newLocationMessage',function(message){
+    console.log('New location message!',message); //custom event-listener!
     var formattedTime = moment(message.createdAt).format('h:mm A');
-    var li = jQuery('<li></li>');
-    var a = jQuery('<a target="_blank">My current location<a/>') //_blank opens the link in new tab, so we dont get kicked out of the room
-    li.text(`${message.from} ${formattedTime}:`);
-    a.attr('href',message.url);
-    li.append(a);
-    jQuery('#messages').append(li)
+    var template = jQuery('#location-message-template').html();
+    var html = Mustache.render(template,{
+        url:message.url,
+        from:message.from,
+        createdAt:formattedTime
+    });
+    jQuery('#messages').append(html);
 });
 
 // socket.emit('createMessage',{
